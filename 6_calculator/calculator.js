f847c35709fc22145b257860f0a6390d0889a8ae
vojinnk <vojodjukanovic@gmail.com>
2020-11-14 22:23:52 +0100

    some css fixes, calculation on mouse click done, some fixes neede for click on entry button

--- 6_calculator/calculator.js
+++ 6_calculator/calculator.js
@@ -1,9 +1,36 @@
 var container = document.querySelector(".container");
 container.addEventListener("click",clicked);
+var equal = document.getElementById("equal");
+equal.addEventListener("click",calculate)
 
+var entry = document.getElementById("entry").firstChild;
+var showRes =document.getElementById("entry").lastChild;
+//console.log(entry);
 
 function clicked(e){
     
-    var entry = document
+    
+   // console.log(e.target.id);
+   if(e.target.id==="equal")
+   {   
+        var result = eval(entry.innerText);
+        //console.log(result);
+        entry.innerText += "=" + result; 
+    
+   }
+   else if(e.target.id === "c"){
+    //console.log("probaaa");
+    entry.innerText = "";
+   }else {
+       entry.innerText += e.target.innerText; 
+   }
+
+    
+    
+}
+function calculate(e){
+    e.stopPropagation();
+     
+    
 }
 
