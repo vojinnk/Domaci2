de114047b2c43338e8159852b635cb9adfe4863b
vojinnk <vojodjukanovic@gmail.com>
2020-11-15 21:32:31 +0100

    added keyboard funcitonality, 90% done, only double dot has to be fixed!

--- 6_calculator/calculator.js
+++ 6_calculator/calculator.js
@@ -8,36 +8,28 @@ var buttons = document.querySelectorAll(".button");
 buttons.forEach(elem=>{
     elem.addEventListener("click",clicked);
 })
+
+document.addEventListener("keyup",keyPressed);
+
 var entry = document.getElementById("entry");
 var calculated=true;
 var dot = false;
-function calculator(input){
-    
-}
 
-function clicked(e){
-    e.stopPropagation();
-    //if you want to reset to new calculation every time uncoment this below;
-    /*if(calculated===true){
-        entry.innerText="";
-        calculated=false;
-    }*/
-   
-    //console.log(e.target.id!=="equal" && e.target.id!=="c");
-    if(e.target.id!=="equal" && e.target.id!=="c"){
-        if(entry.innerText.length>=96){
+function calculator(key,value){
+    if(key!=="equal" && key!=="c"){
+        if(entry.innerText.length>=80){
             alert("too many characters");
             return; 
         }
-        if(e.target.id!=="plus" && e.target.id!=="minus" && e.target.id!=="mul" && e.target.id!=="divide"){
+        if(key!=="plus" && key!=="minus" && key!=="mul" && key!=="divide"){
             //console.log(e.target);
             
         if(calculated===true){
             calculated=false;
-            entry.innerText= e.target.innerText;
+            entry.innerText= value;
         }
         else{
-            entry.innerText+= e.target.innerText;
+            entry.innerText+= value;
         }
             
         }else {
@@ -50,13 +42,13 @@ function clicked(e){
             if(entry.innerText[entry.innerText.length-1]==="+" || entry.innerText[entry.innerText.length-1]==="-" || 
             entry.innerText[entry.innerText.length-1]==="*" || entry.innerText[entry.innerText.length-1]==="/" ){
                 var newRes= entry.innerText.slice(0,entry.innerText.length-1);
-                entry.innerText=newRes + e.target.innerText;
+                entry.innerText=newRes + value;
             }else{
-                entry.innerText+=e.target.innerText;
+                entry.innerText+=value;
             }
         }
     }else {
-        if(e.target.id==="c"){
+        if(key==="c"){
             //if we want clear then use this
             if(calculated===true){
                 entry.innerText="";
@@ -66,7 +58,7 @@ function clicked(e){
                 entry.innerText=entry.innerText.slice(0,length-1);
             }
         }
-        if(e.target.id==="equal"){
+        if(key==="equal"){
             //if last entry is operand, we make alert
             if(entry.innerText[entry.innerText.length-1]==="+" || entry.innerText[entry.innerText.length-1]==="-" || 
             entry.innerText[entry.innerText.length-1]==="*" || entry.innerText[entry.innerText.length-1]==="/" ){
@@ -84,5 +76,83 @@ function clicked(e){
         }
     }
 }
+function clicked(e){
+    e.stopPropagation();
+    //if you want to reset to new calculation every time uncoment this below;
+    /*if(calculated===true){
+        entry.innerText="";
+        calculated=false;
+    }*/
+   
+    //console.log(key!=="equal" && key!=="c");
+    calculator(e.target.id,e.target.innerText);
+    
+}
 
+function keyPressed(e){
+   // console.log(e.key)
+    switch (e.key){
+        case "0":
+            calculator("zero",e.key);
+            break;
+        case "1":
+            calculator("one",e.key);
+            break;
+
+        case "2":
+            calculator("two",e.key);
+            break;
+        case "3":
+            calculator("three",e.key);
+            break;
+        case "4":
+            calculator("four",e.key);
+            break;
+        case "5":
+            calculator("5",e.key);
+            break;
+        
+        case "6":
+            calculator("six",e.key);
+            break;
+        case "7":
+            calculator("seven",e.key);
+            break;
+
+        case "8":
+            calculator("eight",e.key);
+            break;
+        case "9":
+            calculator("nine",e.key);
+            break;          
+        case ".":
+            calculator("dot",e.key);
+            break;
+        case "+":
+            calculator("plus",e.key);
+            break;
+        case "-":
+            calculator("minus",e.key);
+            break;
+        case "*":
+            calculator("mul",e.key);
+            break;
+            
+        case "/":
+            calculator("divide",e.key);
+            break;
+        case "=":
+        case "Enter":
+            calculator("equal","=");
+            break;
+        case "C":
+        case "c":
+        case "Delete":
+        case "Backspace":    
+            calculator("c","c");
+            break;
+        default: 
+            console.log(e.key)
+    }
+}
 
