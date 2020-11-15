ab6e4b6ebdaff1495bafabf213bcf1f04673ece6
vojinnk <vojodjukanovic@gmail.com>
2020-11-15 18:45:36 +0100

    Calculations done great, need to validate for double dot, and add entry through keyboard, also positioned it to center

--- 6_calculator/calculator.js
+++ 6_calculator/calculator.js
@@ -1,36 +1,64 @@
-var container = document.querySelector(".container");
+/*var container = document.querySelector(".container");
 container.addEventListener("click",clicked);
 var equal = document.getElementById("equal");
 equal.addEventListener("click",calculate)
-
-var entry = document.getElementById("entry").firstChild;
-var showRes =document.getElementById("entry").lastChild;
+*/
 //console.log(entry);
+var buttons = document.querySelectorAll(".button");
+buttons.forEach(elem=>{
+    elem.addEventListener("click",clicked);
+})
+var entry = document.getElementById("entry");
+var calculated=true;
 
 function clicked(e){
-    
-    
-   // console.log(e.target.id);
-   if(e.target.id==="equal")
-   {   
-        var result = eval(entry.innerText);
-        //console.log(result);
-        entry.innerText += "=" + result; 
-    
-   }
-   else if(e.target.id === "c"){
-    //console.log("probaaa");
-    entry.innerText = "";
-   }else {
-       entry.innerText += e.target.innerText; 
-   }
-
-    
-    
-}
-function calculate(e){
     e.stopPropagation();
-     
-    
+    if(calculated===true){
+        entry.innerText="";
+        calculated=false;
+    }
+   
+    //console.log(e.target.id!=="equal" && e.target.id!=="c");
+    if(e.target.id!=="equal" && e.target.id!=="c"){
+        if(entry.innerText.length>=96){
+            alert("too many characters");
+            return; 
+        }
+        
+        if(e.target.id!=="plus" && e.target.id!=="minus" && e.target.id!=="mul" && e.target.id!=="divide"){
+            //console.log(e.target);
+            entry.innerText+= e.target.innerText;
+            
+        }else {
+           // console.log(entry.innerText[entry.innerText.length-1]);
+            if(entry.innerText[entry.innerText.length-1]==="+" || entry.innerText[entry.innerText.length-1]==="-" || 
+            entry.innerText[entry.innerText.length-1]==="*" || entry.innerText[entry.innerText.length-1]==="/" ){
+                var newRes= entry.innerText.slice(0,entry.innerText.length-1);
+                entry.innerText=newRes + e.target.innerText;
+            }else{
+                entry.innerText+=e.target.innerText;
+            }
+        }
+    }else {
+        if(e.target.id==="c"){
+            //if we want clear then use this
+            //entry.innerText="";
+            //if you want to do back use tthis
+            entry.innerText=entry.innerText.slice(0,length-1);
+        }
+        if(e.target.id==="equal"){
+            //if last entry is operand, we make alert
+            if(entry.innerText[entry.innerText.length-1]==="+" || entry.innerText[entry.innerText.length-1]==="-" || 
+            entry.innerText[entry.innerText.length-1]==="*" || entry.innerText[entry.innerText.length-1]==="/" ){
+                alert("Your input is not finished, remove operand or add number after it!!")
+            }else{
+                
+                var res = eval(entry.innerText);
+                entry.innerText=res;
+                calculated=true;
+            }
+        }
+    }
 }
+
 
