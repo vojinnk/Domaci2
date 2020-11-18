5954ef82696742facf3d86ce7a7f952005796735
vojinnk <vojodjukanovic@gmail.com>
2020-11-18 21:36:30 +0100

    update

--- 3_Boxes/boxes.js
+++ 3_Boxes/boxes.js
@@ -1,4 +1,5 @@
-
+var boxes;
+var x;
 var userEntry="";
 while(userEntry==="" || userEntry===null){
     var userEntry=prompt("Enter number of boxes","10");
@@ -20,17 +21,29 @@ function addBox(){
     var newBox = document.createElement("div");
     newBox.classList.add("box");
     var container = document.querySelector(".container");
+    newBox.innerHTML="<i class=\"material-icons close\" >close</i>";
+    newBox.addEventListener("click",clicked,{capture: false});
     container.appendChild(newBox);
-    boxes = document.querySelectorAll(".box");
-    boxFunctionality();
+  
     
 }
 
+/*
+function removeBox(e){
+    
+}
 function boxFunctionality(){
+    boxes = document.querySelectorAll(".box");
+    x = document.querySelectorAll(".close");
     boxes.forEach(elem=>{
         elem.addEventListener("click",clicked);
     })
-}
+    
+    x.forEach(elem => {
+        elem.addEventListener("click",removeBox);
+    })
+    console.log(x);
+}*/
 
 
 document.addEventListener("click", function(e){
@@ -43,14 +56,21 @@ document.addEventListener("click", function(e){
 
 
 function clicked(e){
+    e.stopPropagation();
+   //console.log(e.target.classList.contains("material-icons"));
+   if(e.target.classList.contains("material-icons")) return;
+    //console.log(e.eventPhase);
+    
+
     var active=document.querySelectorAll(".active");
     if(active.length!==0){
         active[0].classList.remove("active");
     }
-    e.stopPropagation();
+  
     //console.log(e.target);
     e.target.classList.add("active");
     var par=e;
+    
     document.addEventListener("keyup",inputF);
 }
 function inputF(e){
@@ -58,6 +78,7 @@ function inputF(e){
     var active = document.querySelector(".active");
     if((e.keyCode >= 65 && e.keyCode < 91) || e.keyCode===32){
         active.innerText=e.key;
+        active.innerHTML+="<i class=\"material-icons close\" >close</i>";
         
     }else{
         alert("Wrong input");
