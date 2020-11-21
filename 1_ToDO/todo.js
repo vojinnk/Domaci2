e650c12cd3e3e2c3eed294da7dfbef7201f66cfb
vojinnk <vojodjukanovic@gmail.com>
2020-11-21 23:17:00 +0100

    search and add done, next to do removing item;

--- 1_ToDO/todo.js
+++ 1_ToDO/todo.js
@@ -3,6 +3,7 @@ var insertBox = document.querySelector("#addForm");
 
 //console.log(searchBox);
 searchBox.addEventListener("keyup",search);
+addForm.addEventListener("submit",insertItem);
 
 function search(e){
     var items = Array.from(document.querySelectorAll("li"));
@@ -12,12 +13,27 @@ function search(e){
    //console.log(input)
     items.forEach(elem=>{
         //console.log(elem.firstChild);
-        if(elem.firstChild.includes(input)){
-            
-            elem.style.display=block;
+        if(elem.firstChild.textContent.toLowerCase().includes(input)){
+            elem.style.display="block";
         }else{
-            elem.style.display=none;
+            elem.style.display="none";
 
-    })
+    }
     
+    })
+}
+function insertItem(e){
+    e.preventDefault();
+    
+    var name = document.querySelector("#item").value;
+    var li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerText=name;
+    var del = document.createElement("button");
+    //classlist.add dodaje samo po jednu!
+    del.className="btn btn-danger btn-sm float-right delete";
+    del.innerText="X";
+    li.appendChild(del);
+    document.querySelector("#items").appendChild(li);
+
 }
