6d639eac68ba478fc6381445385890ab298bf3c4
vojinnk <vojodjukanovic@gmail.com>
2020-11-22 23:53:59 +0100

    some of the functions done,gona do others later

---
+++ 7.Callbacks_closures_timers/timers.js
@@ -0,0 +1,142 @@
+/* CHALLENGE 1 */
+
+function sayHowdy() {
+    console.log('Howdy');
+  }
+  
+  function testMe() {
+    setTimeout(sayHowdy, 0);
+    console.log('Partnah');
+  }
+  // After thinking it through, uncomment the following line to check your guess!
+   //testMe(); // what order should these log out? Howdy or Partnah first?
+  
+  
+  /* CHALLENGE 2 */
+  
+  function delayedGreet() {
+    // ADD CODE HERE
+    setTimeout(()=>{
+        console.log("welcome");
+    },3000)
+  }
+  // Uncomment the following line to check your work!
+  //delayedGreet(); // should log (after 3 seconds): welcome
+  
+  
+  /* CHALLENGE 3 */
+  
+  function helloGoodbye() {
+    // ADD CODE HERE
+    console.log("hello");
+    setTimeout(()=>{
+        console.log("good bye");
+    },2000)
+  }
+  // Uncomment the following line to check your work!
+   //helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+  
+  
+  /* CHALLENGE 4 */
+  
+  function brokenRecord() {
+    // ADD CODE HERE
+    setInterval(()=>{
+      console.log("hi again");
+    },1000)
+  }
+  // Uncomment the following line to check your work!
+  // brokenRecord(); // should log (every second): hi again
+  
+  
+  /* CHALLENGE 5 */
+  
+  function limitedRepeat() {
+    // ADD CODE HERE
+    //resenje bez setTimeouta
+    var counter = 0;
+    var repeat = setInterval(()=>{
+      console.log("hi for now");
+      counter++;
+      if(counter>=5){
+        clearInterval(repeat);
+      }
+    },1000)
+  }
+  // Uncomment the following line to check your work!
+  //limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+  
+  
+  /* CHALLENGE 6 */
+  
+  function everyXsecsForYsecs(func ,x,y) {
+    var repeat = setInterval(func,x*1000);
+    setTimeout(()=>{
+      clearInterval(repeat)
+    },y*1000);
+    // ADD CODE HERE
+  }
+  // Uncomment the following lines to check your work!
+   function theEnd() {
+     console.log('This is the end!');
+   }
+   //everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+  
+  
+  /* CHALLENGE 7 */
+  
+  function delayCounter(target, wait) {
+  
+  }
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  // const countLogger = delayCounter(3, 1000)
+  // countLogger();
+  // After 1 second, log 1
+  // After 2 seconds, log 2
+  // After 3 seconds, log 3
+  
+  /* CHALLENGE 8 */
+  
+  function promised (val) {
+    // ADD CODE HERE
+  }
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  // const createPromise = promised('wait for it...');
+  // createPromise.then((val) => console.log(val)); 
+  // will log "wait for it..." to the console after 2 seconds
+  
+  /* CHALLENGE 9 */
+  
+  class SecondClock {
+    constructor(cb) {
+      // ADD CODE HERE
+    }
+    // ADD METHODS HERE
+  }
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  // const clock = new SecondClock((val) => { console.log(val) });
+  // console.log("Started Clock.");
+  // clock.start();
+  // setTimeout(() => {
+  //     clock.reset();
+  //     console.log("Stopped Clock after 6 seconds.");
+  // }, 6000);
+  
+  /* CHALLENGE 10 */
+  
+  function debounce(callback, interval) {
+    // ADD CODE HERE
+  }
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+  // function giveHi() { return 'hi'; }
+  // const giveHiSometimes = debounce(giveHi, 3000);
+  // console.log(giveHiSometimes()); // -> 'hi'
+  // setTimeout(function() { console.log(giveHiSometimes()); }, 2000); // -> undefined
+  // setTimeout(function() { console.log(giveHiSometimes()); }, 4000); // -> undefined
+  // setTimeout(function() { console.log(giveHiSometimes()); }, 8000); // -> 'hi'
+  
+  
