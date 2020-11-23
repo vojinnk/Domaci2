bde0f8ea1ecc29420640dee8484010d7f78cbfd0
vojinnk <vojodjukanovic@gmail.com>
2020-11-24 00:56:08 +0100

    almost all closures done!!!

--- 7.Callbacks_closures_timers/closures.js
+++ 7.Callbacks_closures_timers/closures.js
@@ -4,7 +4,7 @@
 // CHALLENGE 1
 function createFunction() {
   	
-    function challone(){
+  function challone(){
     console.log("hello");
   }
 return challone;
@@ -18,10 +18,10 @@ return challone;
 
 // CHALLENGE 2
 function createFunctionPrinter(input) {
-   function challtwo(){
+ function challtwo(){
    console.log(input);
  }
-  return challtwo;
+return challtwo;
 }	
 
 // /*** Uncomment these to check your work! ***/
@@ -57,7 +57,7 @@ const jasCounter = outer();
 
 
 function addByX(x) {
-  function challthree(input){
+function challthree(input){
   return x+input;
 }
 return challthree;
@@ -80,7 +80,7 @@ const addByTwo = addByX(2);
 
 // CHALLENGE 4
 function once(func) {
-  var counter =1;
+var counter =1;
 var firstRes;
 function once(input){
   if(counter===1){
@@ -105,7 +105,7 @@ return once;
 
 // CHALLENGE 5
 function after(count, func) {
-  var c=0;
+var c=0;
 function chalfive(){
   c++;
   if(c>=3){
@@ -126,17 +126,17 @@ const afterCalled = after(3, called);
 
 // CHALLENGE 6
 function delay(func, wait) {
-  setTimeout(()=>func(),wait);
+setTimeout(()=>func(),wait);
 }
 
 
 // CHALLENGE 7
 function rollCall(names) {
-  var c=0;
+var c=0;
 function all(){
   if(c<names.length){
-        console.log(names[c]);
-        c++;
+      console.log(names[c]);
+      c++;
   }else console.log("Everyone accounted for")
 }
 return all;
@@ -152,13 +152,13 @@ const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {
-    var all=[];
-    function cheight(input){
+  var all=[];
+  function cheight(input){
     if(input===magicWord){
       return all;
     }else{
       var obj={};
-        obj[input]=func(input);
+      obj[input]=func(input);
       all.push(obj);
       return func(input);
     }
@@ -177,10 +177,10 @@ return cheight;
 
 // CHALLENGE 9
 function cycleIterator(array) {
-      var c=0;
-    function it(){
-        if(c<array.length){
-          var p=c;
+  var c=0;
+  function it(){
+      if(c<array.length){
+        var p=c;
         c++;
         return array[p];
       }else{
@@ -203,7 +203,7 @@ return it;
 // CHALLENGE 10
 //not sure that i done what is asked for!!! mozda ...args umjesto input tj ...input???
 function defineFirstArg(func, arg) {
-  function df(input){
+function df(input){
   return func(arg,input);
 }
 return df;
@@ -217,7 +217,7 @@ return df;
 
 // CHALLENGE 11
 function dateStamp(func) {
-  var date = Date(Date.now());
+var date = Date(Date.now());
 function chalel(...arg){
   var output = func(...arg);
   return {
@@ -267,19 +267,28 @@ return chas;
 
 // CHALLENGE 13
 function createSecretHolder(secret) {
-
+var s=secret;
+return {
+  setSecret:function(arg){s=arg;},
+  getSecret:function(){return s}
+}
 }
 
 // /*** Uncomment these to check your work! ***/
-// obj = createSecretHolder(5)
-// obj.getSecret() // => returns 5
-// obj.setSecret(2)
-// obj.getSecret() // => returns 2
+// var obj = createSecretHolder(5)
+//console.log(obj.getSecret()) // => returns 5
+//obj.setSecret(2)
+//console.log(obj.getSecret()) // => returns 2
 
 
 // CHALLENGE 14
 function callTimes() {
-
+var c=0;
+function called(){
+  c++;
+  return c; 
+}
+return called
 }
 
 // /*** Uncomment these to check your work! ***/
@@ -293,7 +302,17 @@ function callTimes() {
 
 // CHALLENGE 15
 function russianRoulette(num) {
-
+var c=1;
+function play(){
+  if(c<num){
+    c++;
+    return "click";
+  }else if(c===num){
+    c++;
+    return "bang";
+  }else return "reload to play again";
+}
+return play;
 }
 
 // /*** Uncomment these to check your work! ***/
@@ -307,7 +326,22 @@ function russianRoulette(num) {
 
 // CHALLENGE 16
 function average() {
-
+var avg=0;
+var all=[];
+function average(input){
+  if(arguments.length===0){
+    return avg;
+  }else{
+    all.push(input);
+    var sum=0;
+    all.forEach(elem=>{
+      sum+=elem;
+    });
+    avg=sum/all.length;
+    return avg;
+  }
+}
+return average;
 }
 
 // /*** Uncomment these to check your work! ***/
@@ -315,80 +349,142 @@ function average() {
 // console.log(avgSoFar()); // => should log 0
 // console.log(avgSoFar(4)); // => should log 4
 // console.log(avgSoFar(8)); // => should log 6
-// console.log(avgSoFar()); // => should log 6
-// console.log(avgSoFar(12)); // => should log 8
-// console.log(avgSoFar()); // => should log 8
+//console.log(avgSoFar()); // => should log 6
+//console.log(avgSoFar(12)); // => should log 8
+//console.log(avgSoFar()); // => should log 8
 
 
 // CHALLENGE 17
 function makeFuncTester(arrOfTests) {
 
+function test(cb){
+  var isTrue=true;
+  arrOfTests.forEach(elem=>{
+    
+    //console.log(elem[0]);
+    //console.log(elem[1]);
+   // console.log(cb(elem[0])!==elem[1]);
+    if(cb(elem[0])!==elem[1]){
+     // console.log("enter");
+      isTrue=false;
+    }        
+    
+  })
+  return isTrue;
+}
+return test;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const capLastTestCases = [];
-// capLastTestCases.push(['hello', 'hellO']);
-// capLastTestCases.push(['goodbye', 'goodbyE']);
-// capLastTestCases.push(['howdy', 'howdY']);
-// const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
-// const capLastAttempt1 = str => str.toUpperCase();
-// const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
-// console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
-// console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
-
+/* const capLastTestCases = [];
+capLastTestCases.push(['hello', 'hellO']);
+capLastTestCases.push(['goodbye', 'goodbyE']);
+capLastTestCases.push(['howdy', 'howdY']);
+const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+const capLastAttempt1 = str => str.toUpperCase();
+const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
+console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
+*/
 
 // CHALLENGE 18
 function makeHistory(limit) {
-
+var history=[];
+function work(str){
+  if(str==="undo"){
+    if(history.length>0){
+      var p = history.pop();
+      return p + " undone"
+    }else return "nothing to undo"
+  }else{
+    if(history.length>=limit){
+      history.shift();
+      history.push(str);
+      return str + " done";
+    }else{
+      history.push(str);
+      return str + " done"
+    }
+  }
+}
+return work;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const myActions = makeHistory(2);
-// console.log(myActions('jump')); // => should log 'jump done'
-// console.log(myActions('undo')); // => should log 'jump undone'
-// console.log(myActions('walk')); // => should log 'walk done'
-// console.log(myActions('code')); // => should log 'code done'
-// console.log(myActions('pose')); // => should log 'pose done'
-// console.log(myActions('undo')); // => should log 'pose undone'
-// console.log(myActions('undo')); // => should log 'code undone'
-// console.log(myActions('undo')); // => should log 'nothing to undo'
+/* const myActions = makeHistory(2);
+console.log(myActions('jump')); // => should log 'jump done'
+console.log(myActions('undo')); // => should log 'jump undone'
+console.log(myActions('walk')); // => should log 'walk done'
+console.log(myActions('code')); // => should log 'code done'
+console.log(myActions('pose')); // => should log 'pose done'
+console.log(myActions('undo')); // => should log 'pose undone'
+console.log(myActions('undo')); // => should log 'code undone'
+console.log(myActions('undo')); // => should log 'nothing to undo'
 
-
+*/
 // CHALLENGE 19
 function blackjack(array) {
-
+var c=0;
+function dealer(num1,num2){
+ if(c>=array.length) c=0;
+  var pl=true;
+  var bust=false;
+  var sum=0
+  function player(){
+   if(!bust){
+       if(pl){
+         pl=false;
+         sum=num1+num2;
+       return num1+num2;
+     }else{
+       sum+=array[c]
+       c++;
+       if(sum<=21){
+         return sum;
+       }else{
+         bust=true;
+         return "bust";
+       }
+     }
+   }else{
+     return "you are done"
+   }
+ }
+  return player;
+}
+return dealer;
 }
 
 // /*** Uncomment these to check your work! ***/
 
 // /*** DEALER ***/
-// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
 
-// /*** PLAYER 1 ***/
-// const i_like_to_live_dangerously = deal(4, 5);
-// console.log(i_like_to_live_dangerously()); // => should log 9
-// console.log(i_like_to_live_dangerously()); // => should log 11
-// console.log(i_like_to_live_dangerously()); // => should log 17
-// console.log(i_like_to_live_dangerously()); // => should log 18
-// console.log(i_like_to_live_dangerously()); // => should log 'bust'
-// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
-// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+/*** PLAYER 1 ***/
+const i_like_to_live_dangerously = deal(4, 5);
+console.log(i_like_to_live_dangerously()); // => should log 9
+console.log(i_like_to_live_dangerously()); // => should log 11
+console.log(i_like_to_live_dangerously()); // => should log 17
+console.log(i_like_to_live_dangerously()); // => should log 18
+console.log(i_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
 
 // /*** BELOW LINES ARE FOR THE BONUS ***/
 
-// /*** PLAYER 2 ***/
-// const i_TOO_like_to_live_dangerously = deal(2, 2);
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
-// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+/*** PLAYER 2 ***/
+const i_TOO_like_to_live_dangerously = deal(2, 2);
+console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
 
-// /*** PLAYER 3 ***/
-// const i_ALSO_like_to_live_dangerously = deal(3, 7);
-// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
-// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
-// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
-// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
-// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+/*** PLAYER 3 ***/
+const i_ALSO_like_to_live_dangerously = deal(3, 7);
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
